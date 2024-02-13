1ef78323776e4214efac09b48025659b51b8aaea
tolnabert <tolnabert@gmail.com>
2024-02-13 15:02:55 +0100

    adds 2 doors, 1 sword, 1 key to the map for sketch

--- src/main/java/com/codecool/dungeoncrawl/data/inventory/Key.java
+++ src/main/java/com/codecool/dungeoncrawl/data/inventory/Key.java
@@ -1,7 +1,14 @@
 package com.codecool.dungeoncrawl.data.inventory;
 
+import com.codecool.dungeoncrawl.data.Cell;
+
 public class Key extends Item{
-    public Key(String name) {
-        super(name);
+    public Key(String name, Cell cell) {
+        super(name, cell);
+    }
+
+    @Override
+    public String getTileName() {
+        return "key";
     }
 }
