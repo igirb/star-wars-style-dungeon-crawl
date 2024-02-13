7318e40f6491d53a613266c3ebd68065ce03bc32
igirb <139497578+igirb@users.noreply.github.com>
2024-02-13 13:47:52 +0100

    old version/no changes/

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -4,7 +4,7 @@ import com.codecool.dungeoncrawl.data.Cell;
 
 public class Player extends Actor {
     public Player(Cell cell) {
-        super(cell);
+    super(cell);
     }
 
     public String getTileName() {
